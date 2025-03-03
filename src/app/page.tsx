ff5cecb3378458833412b3c325a3ee4d472d5c65
tommy10092525 <tommyxla777@gmail.com>
2025-03-03 16:58:57 +0900

    Update menu sheet styling with lighter background colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -216,12 +216,12 @@ export default function Home() {
         </SheetTrigger>
         <SheetContent className="bg-white/70 dark:bg-black/30">
           <SheetTitle className="text-center mt-3 text-xl">Menu</SheetTitle>
-          <a className="mx-10 hover:underline text-black dark:text-white bg-black/80 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto"
+          <a className="mx-10 hover:underline text-black dark:text-white bg-white/60 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto"
             href='https://docs.google.com/forms/d/e/1FAIpQLScPysPRj60-S2v_zmFjrQF6YKlS0Qe200GSO4LnEMsiVbXxYg/viewform?usp=sf_link'>アプリご意見</a>
-          <p className="mx-10 share-btn hover:underline text-black dark:text-white bg-black/80 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto">アプリを共有</p>
-          <a className="mx-10 hover:underline text-black dark:text-white bg-black/80 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto"
+          <p className="mx-10 share-btn hover:underline text-black dark:text-white bg-white/60 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto">アプリを共有</p>
+          <a className="mx-10 hover:underline text-black dark:text-white bg-white/60 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto"
             href='https://codemates123.github.io/homepage/'>CODE MATESとは</a>
-          <a className="mx-10 hover:underline text-black dark:text-white bg-black/80 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto"
+          <a className="mx-10 hover:underline text-black dark:text-white bg-white/60 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto"
             href='https://www.instagram.com/codemates_hosei?igsh=MTJvcmthMzUwOW90cg=='>Instagram</a>
         </SheetContent>
 
