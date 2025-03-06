c488879d864f12c6b60925fbabba1fb9193af181
tommy10092525 <tommyxla777@gmail.com>
2025-03-07 04:42:31 +0900

    Update discount page styling and add link to Fuji restaurant details

--- src/app/discount/page.tsx
+++ src/app/discount/page.tsx
@@ -8,7 +8,7 @@ import hicheeseImage from "../../../public/hicheese.jpg"
 import kokuterudoImage from "../../../public/kokuterudou.jpg"
 const page = () => {
   return (
-    <div className='bg-white dark:bg-black text-black dark:text-white min-h-screen'>
+    <div className='bg-zinc-100 dark:bg-zinc-950 text-black dark:text-white min-h-screen'>
       <header className='bg-rose-500 text-white py-4 fixed top-0 left-0 right-0 z-10 px-4'>
         <Link href="/" className='float-left'>
           <Image src={logo} alt="たまっぷのロゴ" width={400} height={400} className='w-16 h-16 -my-4' />
@@ -16,7 +16,7 @@ const page = () => {
         <h1 className='text-center text-2xl font-bold'>たまっぷ 提携店舗一覧</h1>
       </header>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 mt-20'>
-        <Link href="#" className='border-2 rounded-lg border-rose-500 shadow-lg hover:scale-105 transition-all duration-300'>
+        <Link href="/discount/fuji" className='bg-gray-100 dark:bg-zinc-900 border-2 rounded-lg border-rose-500 shadow-lg hover:scale-105 transition-all duration-300'>
           <Image src={fujiImage} alt="うどん屋　藤の画像" width={500} height={500} className='w-3xl h-56 mx-auto' />
           <div className='text-center'>
             <p className='text-md'>めじろ台　うどん屋</p>
@@ -24,7 +24,7 @@ const page = () => {
             <p className='text-rose-500'>普通盛り50円引き<br />大盛無料</p>
           </div>
         </Link>
-        <Link href="#" className='border-2 rounded-lg border-rose-500 shadow-lg hover:scale-105 transition-all duration-300'>
+        <Link href="#" className='bg-gray-100 dark:bg-zinc-900 border-2 rounded-lg border-rose-500 shadow-lg hover:scale-105 transition-all duration-300'>
           <Image src={hicheeseImage} alt="ハイチーズの画像" width={500} height={500} className='w-3xl h-56 mx-auto' />
           <div className='text-center'>
             <p className='text-md'>八王子　チーズ料理</p>
@@ -32,7 +32,7 @@ const page = () => {
             <p className='text-rose-500'>ランチソフトドリンク無料<br />コースディナー500円引き<br />飲み放題30分延長</p>
           </div>
         </Link>
-        <Link href="#" className='border-2 rounded-lg border-rose-500 shadow-lg hover:scale-105 transition-all duration-300'>
+        <Link href="#" className='bg-gray-100 dark:bg-zinc-900 border-2 rounded-lg border-rose-500 shadow-lg hover:scale-105 transition-all duration-300'>
           <Image src={goemonImage} alt="吾衛門の画像" width={500} height={500} className='w-3xl h-56 mx-auto' />
           <div className='text-center'>
             <p className='text-md'>西八王子　ラーメン店</p>
@@ -40,7 +40,7 @@ const page = () => {
             <p className='text-rose-500'>大盛無料</p>
           </div>
         </Link>
-        <Link href="#" className='border-2 rounded-lg border-rose-500 shadow-lg hover:scale-105 transition-all duration-300'>
+        <Link href="#" className='bg-gray-100 dark:bg-zinc-900 border-2 rounded-lg border-rose-500 shadow-lg hover:scale-105 transition-all duration-300'>
             <Image src={kokuterudoImage} alt="コクテル堂の画像" width={500} height={500} className='w-3xl h-56 mx-auto'/>
           <div className='text-center'>
             <p className='text-md'>橋本　カフェ</p>
