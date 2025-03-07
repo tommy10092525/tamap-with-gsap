127b8fe4e9229d6e2abbfeddc8e49816c9d4293a
tommy10092525 <tommyxla777@gmail.com>
2025-03-08 01:12:17 +0900

    Update discount page dark mode border styling for partner store cards

--- src/app/discount/page.tsx
+++ src/app/discount/page.tsx
@@ -16,7 +16,7 @@ const page = () => {
         <h1 className='text-center text-2xl font-bold'>たまっぷ 提携店舗一覧</h1>
       </header>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-8 mt-16'>
-        <Link href="/discount/fuji" className='bg-gray-100 dark:bg-zinc-900 border-0 dark:border-2 dark:border-white rounded-lg shadow-lg hover:scale-105 transition-all duration-300'>
+        <Link href="/discount/fuji" className='bg-gray-100 dark:bg-zinc-900 border-0 dark:border dark:border-zinc-700 rounded-lg shadow-lg hover:scale-105 transition-all duration-300'>
           <Image src={fujiImage} alt="うどん屋　藤の画像" width={500} height={500} className='mx-auto rounded-t-lg' />
           <div className='text-center'>
             <p className='text-md'>めじろ台　うどん屋</p>
@@ -24,7 +24,7 @@ const page = () => {
             <p className='text-rose-500'>普通盛り50円引き<br />大盛無料</p>
           </div>
         </Link>
-        <Link href="/discount/hicheese" className='bg-gray-100 dark:bg-zinc-900 border-0 dark:border-2 dark:border-white rounded-lg shadow-gray shadow-lg hover:scale-105 transition-all duration-300'>
+        <Link href="/discount/hicheese" className='bg-gray-100 dark:bg-zinc-900 border-0 dark:border dark:border-zinc-700 rounded-lg shadow-lg hover:scale-105 transition-all duration-300'>
           <Image src={hicheeseImage} alt="ハイチーズの画像" width={500} height={500} className='mx-auto rounded-t-lg' />
           <div className='text-center'>
             <p className='text-md'>八王子　チーズ料理</p>
@@ -32,7 +32,7 @@ const page = () => {
             <p className='text-rose-500'>ランチソフトドリンク無料<br />コースディナー500円引き<br />飲み放題30分延長</p>
           </div>
         </Link>
-        <Link href="/discount/goemon" className='bg-gray-100 dark:bg-zinc-900 border-0 dark:border-2 dark:border-white rounded-lg shadow-gray shadow-lg hover:scale-105 transition-all duration-300'>
+        <Link href="/discount/goemon" className='bg-gray-100 dark:bg-zinc-900 border-0 dark:border dark:border-zinc-700 rounded-lg shadow-lg hover:scale-105 transition-all duration-300'>
           <Image src={goemonImage} alt="吾衛門の画像" width={500} height={500} className='mx-auto rounded-t-lg' />
           <div className='text-center'>
             <p className='text-md'>西八王子　ラーメン店</p>
@@ -40,7 +40,7 @@ const page = () => {
             <p className='text-rose-500'>大盛無料</p>
           </div>
         </Link>
-        <Link href="/discount/kokuterudou" className='bg-gray-100 dark:bg-zinc-900 border-0 dark:border-2 dark:border-white rounded-lg shadow-gray shadow-lg hover:scale-105 transition-all duration-300'>
+        <Link href="/discount/kokuterudou" className='bg-gray-100 dark:bg-zinc-900 border-0 dark:border dark:border-zinc-700 rounded-lg shadow-lg hover:scale-105 transition-all duration-300'>
           <Image src={kokuterudoImage} alt="コクテル堂の画像" width={500} height={500} className='mx-auto rounded-t-lg' />
           <div className='text-center'>
             <p className='text-md'>橋本　カフェ</p>
