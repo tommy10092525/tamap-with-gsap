0244b49a577e14a45ef0abf181c92e0408053d73
tommy10092525 <tommyxla777@gmail.com>
2025-03-08 17:41:56 +0900

    gridレイアウトの設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ import {
 gsap.registerPlugin(useGSAP)
 gsap.registerPlugin(ScrollTrigger)
 gsap.ticker.fps(120)
-gsap.ticker.lagSmoothing(1000,16)
+gsap.ticker.lagSmoothing(1000, 16)
 
 export default function Home() {
   const [state, setState] = useState<State>({ station: "", isComingToHosei: true, menuOpened: false })
@@ -43,8 +43,8 @@ export default function Home() {
   }
   const mainContainer = useRef(null)
   useGSAP(() => {
-    gsap.fromTo(gsap.utils.selector(mainContainer)("div,a"), { autoAlpha: 0}, { autoAlpha: 1, duration: 0.3 ,stagger: 0.1,ease:"none"})
-    gsap.fromTo(timesContainer.current,{height:320},{delay:1,height:160,duration:0.5,ease:"power1.inOut"})
+    gsap.fromTo(gsap.utils.selector(mainContainer)("div,a"), { autoAlpha: 0 }, { autoAlpha: 1, duration: 0.3, stagger: 0.1, ease: "none" })
+    // gsap.fromTo(timesContainer.current,{height:320},{delay:1,height:160,duration:0.5,ease:"power1.inOut"})
   })
   const arrowsRef = useRef(null)
   const departureRef = useRef(null)
@@ -54,20 +54,20 @@ export default function Home() {
     gsap.fromTo(arrowsRef.current, { rotate: 0 }, { rotate: 180, duration: 0.3 })
   })
   const animateDirectionButton = useGSAP().contextSafe(() => {
-    gsap.fromTo(arrowsContainer.current, {scale:1.05}, { scale:1, duration: 0.3 })
+    gsap.fromTo(arrowsContainer.current, { scale: 1.05 }, { scale: 1, duration: 0.3 })
   })
   const timesContainer = useRef(null)
   const directionContainer = useRef(null)
   const overlayContainer = useRef(null)
-  const times={
+  const times = {
     economics: useRef(null),
     health: useRef(null),
     gym: useRef(null),
     sport: useRef(null),
   }
   const animateText = useGSAP().contextSafe(() => {
-    gsap.fromTo(timesContainer.current, { opacity: 0, y: 10 }, { y: 0, duration: 0.3, opacity: 1 ,stagger:0.01})
-    gsap.fromTo(Object.values(times).map(ref => ref.current), { opacity: 0, y: 5 }, { y: 0, duration: 0.3, opacity: 1 ,stagger:0.01})
+    gsap.fromTo(timesContainer.current, { opacity: 0, y: 10 }, { y: 0, duration: 0.3, opacity: 1, stagger: 0.01 })
+    gsap.fromTo(Object.values(times).map(ref => ref.current), { opacity: 0, y: 5 }, { y: 0, duration: 0.3, opacity: 1, stagger: 0.01 })
 
   })
   const stationRefs = {
@@ -78,22 +78,22 @@ export default function Home() {
   const animateStationButton = useGSAP().contextSafe((station: string) => {
     if (station === "nishihachioji") {
       gsap.fromTo(stationRefs.nishihachioji.current, { scale: 1.05 }, { scale: 1, duration: 0.3 })
-      gsap.to(stationRefs.mejirodai.current, { scale: 0.9 ,duration:0.3})
-      gsap.to(stationRefs.aihara.current, { scale: 0.9 ,duration:0.3})
+      gsap.to(stationRefs.mejirodai.current, { scale: 0.9, duration: 0.3 })
+      gsap.to(stationRefs.aihara.current, { scale: 0.9, duration: 0.3 })
     } else if (station == "mejirodai") {
-      gsap.to(stationRefs.nishihachioji.current, { scale: 0.9 ,duration:0.3})
+      gsap.to(stationRefs.nishihachioji.current, { scale: 0.9, duration: 0.3 })
       gsap.fromTo(stationRefs.mejirodai.current, { scale: 1.05 }, { scale: 1, duration: 0.3 })
-      gsap.to(stationRefs.aihara.current, { scale: 0.9 ,duration:0.3})
+      gsap.to(stationRefs.aihara.current, { scale: 0.9, duration: 0.3 })
     } else {
-      gsap.to(stationRefs.nishihachioji.current, { scale: 0.9 ,duration:0.3})
-      gsap.to(stationRefs.mejirodai.current, { scale: 0.9 ,duration:0.3})
+      gsap.to(stationRefs.nishihachioji.current, { scale: 0.9, duration: 0.3 })
+      gsap.to(stationRefs.mejirodai.current, { scale: 0.9, duration: 0.3 })
       gsap.fromTo(stationRefs.aihara.current, { scale: 1.05 }, { scale: 1, duration: 0.3 })
     }
   })
   const waribikiRef = useRef(null)
   useGSAP(() => {
-      gsap.fromTo(waribikiRef.current, { scale: 0.95, duration: 1}, { scale: 1.05, duration: 1, yoyo: true, repeat: -1,ease:"power1.out" })
-  })
+    gsap.fromTo(waribikiRef.current, { scale: 0.95, duration: 1}, { scale: 1.05, duration: 1, yoyo: true, repeat: -1, ease: "power1.out" })
+  },[])
   useEffect(() => {
     if (localStorage.getItem("firstAccessed") !== "false") {
       initUserInput()
@@ -134,7 +134,7 @@ export default function Home() {
     animateDirectionButton()
     animateArrows()
     animateText()
-    gsap.fromTo(directionContainer.current, { rotateY: 180,autoAlpha:0 }, { rotateY: 0, duration: 0.3, autoAlpha:1 })
+    gsap.fromTo(directionContainer.current, { rotateY: 180, autoAlpha: 0 }, { rotateY: 0, duration: 0.3, autoAlpha: 1 })
   }
 
   const handleMenuButtonClicked = () => {
@@ -156,10 +156,10 @@ export default function Home() {
       }
     })
     animateText()
-    if(state.isComingToHosei){
-      gsap.fromTo(departureRef.current, { y: -20 ,autoAlpha:0}, { y: 0, duration: 0.3, autoAlpha:1 })
-    }else{
-      gsap.fromTo(destinationRef.current, { y: -20 ,autoAlpha:0}, { y: 0, duration: 0.3, autoAlpha:1 })
+    if (state.isComingToHosei) {
+      gsap.fromTo(departureRef.current, { y: -20, autoAlpha: 0 }, { y: 0, duration: 0.3, autoAlpha: 1 })
+    } else {
+      gsap.fromTo(destinationRef.current, { y: -20, autoAlpha: 0 }, { y: 0, duration: 0.3, autoAlpha: 1 })
     }
   }
 
@@ -193,7 +193,7 @@ export default function Home() {
       currentHour,
       currentMinutes,
       currentDate: new Date(),
-      length: -3
+      length: -2
     })
     futureBuses = findNextBuses({
       timeTable: structuredClone(timetable),
@@ -204,7 +204,7 @@ export default function Home() {
       currentHour,
       currentMinutes,
       currentDate: new Date(),
-      length: 4
+      length: 3
     })
     const [nextBus] = futureBuses
     if (state.station == "nishihachioji") {
@@ -245,7 +245,7 @@ export default function Home() {
           <SheetTitle className="text-center mt-3 text-xl">Menu</SheetTitle>
           <Link className="mx-10 hover:underline text-black dark:text-white bg-white/60 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto"
             href='https://docs.google.com/forms/d/e/1FAIpQLScPysPRj60-S2v_zmFjrQF6YKlS0Qe200GSO4LnEMsiVbXxYg/viewform?usp=sf_link'>アプリご意見</Link>
-          <p className="mx-10 share-btn hover:underline text-black dark:text-white bg-white/60 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto" onClick={async ()=>{
+          <p className="mx-10 share-btn hover:underline text-black dark:text-white bg-white/60 dark:bg-black/80 dark:border-2 dark:border-white rounded-lg shadow p-2 block text-center will-change-auto" onClick={async () => {
             if (navigator.share) {
               try {
                 await navigator.share({
@@ -265,33 +265,30 @@ export default function Home() {
         </SheetContent>
 
       </Sheet>
-
-
-      <div className="p-3 bg-gradient-to-bl from-sky-400 to-orange-400 dark:from-blue-500 dark:to-orange-400 w-full h-full text-black dark:text-white">
-        <div className="max-w-screen-md mx-auto touch-manipulation p-3" ref={mainContainer}>
-          <Image alt="たまっぷのロゴ" src={logo} className="w-60 h-60 mx-auto -my-8" />
-
+      <div className="p-3 md:p-7 bg-gradient-to-bl from-sky-400 to-orange-400 dark:from-blue-500 dark:to-orange-400 w-full h-full text-black dark:text-white">
+        <Image alt="たまっぷのロゴ" src={logo} className="w-60 h-60 mx-auto -my-8 md:col-span-1" />
+        <div className="mx-auto touch-manipulation p-3 md:grid md:grid-cols-5 gap-6 max-w-5xl" ref={mainContainer}>
           {/* 一つ目のカード */}
-          <div className="-full bg-white/30 dark:bg-black/30 dark:bg-opacity-30 rounded-2xl shadow-lg p-2 mt-4 opacity-0">
+          <div className="-full bg-white/30 dark:bg-black/30 dark:bg-opacity-30 rounded-2xl shadow-lg p-2 mt-4 opacity-0 col-span-2">
 
             {/* 行先表示 */}
-            <div className="text-xl mt-5 font-semibold text-center flex mx-auto" ref={directionContainer}>
-              <p className="js-departure text-center inline-block w-1/2 h-8" ref={departureRef}>{departure}</p>
-              <span className="w-4 h-4">⇒</span>
-              <p className="js-arrival text-center inline-block w-1/2 h-8" ref={destinationRef}>{destination}</p>
+            <div className="text-xl mt-5 font-semibold text-center mx-auto grid grid-cols-5" ref={directionContainer}>
+              <p className="js-departure text-center inline-block h-8 col-span-2" ref={departureRef}>{departure}</p>
+              <p className="h-4 col-span-1">⇒</p>
+              <p className="js-arrival text-center inline-block h-8 col-span-2" ref={destinationRef}>{destination}</p>
             </div>
             {/* 時刻一覧 */}
-            <div className="h-40 overflow-y-scroll text-overflow-ellipsis" ref={timesContainer}>
+            <div className="" ref={timesContainer}>
               {previousBuses.map((item, i) => {
-                return <div className="md:text-2xl text-lg opacity-50 font-sans my-4 flex text-center md:font-bold font-semibold" key={i}>
-                  <p className="w-1/2 mx-auto my-0">{item ? minutesToTime(item.leaveHour * 60 + item.leaveMinute) : "--:--"}</p>
-                  <p className="w-1/2 mx-auto my-0">{item ? minutesToTime(item.arriveHour * 60 + item.arriveMinute) : "--:--"}</p>
+                return <div className="md:text-2xl text-lg opacity-50 font-sans my-4 text-center md:font-bold font-semibold grid grid-cols-2" key={i}>
+                  <p className="w-1/2 mx-auto -my-1">{item ? minutesToTime(item.leaveHour * 60 + item.leaveMinute) : "--:--"}</p>
+                  <p className="w-1/2 mx-auto -my-1">{item ? minutesToTime(item.arriveHour * 60 + item.arriveMinute) : "--:--"}</p>
                 </div>
               })}
               {futureBuses.map((item, i) => {
-                return <div className="md:text-4xl text-3xl font-sans my-4 flex text-center md:font-bold font-semibold" key={i}>
-                  <p className="w-1/2 mx-auto my-0">{item ? minutesToTime(item.leaveHour * 60 + item.leaveMinute) : "--:--"}</p>
-                  <p className="w-1/2 mx-auto my-0">{item ? minutesToTime(item.arriveHour * 60 + item.arriveMinute) : "--:--"}</p>
+                return <div className="md:text-4xl text-3xl font-sans my-4 text-center md:font-bold font-semibold grid grid-cols-2" key={i}>
+                  <p className="w-1/2 mx-auto -my-1">{item ? minutesToTime(item.leaveHour * 60 + item.leaveMinute) : "--:--"}</p>
+                  <p className="w-1/2 mx-auto -my-1">{item ? minutesToTime(item.arriveHour * 60 + item.arriveMinute) : "--:--"}</p>
                 </div>
               })}
             </div>
@@ -307,55 +304,54 @@ export default function Home() {
           </div>
 
           {/* 二つ目のカード */}
-          <div className="w-full bg-white/20 dark:bg-black/30 rounded-2xl shadow-lg relative font-semibold text-lg mt-4 hoverable:hover:scale-110  js-map-container h-auto opacity-0" ref={overlayContainer}>
+          <div className="w-full bg-white/20 dark:bg-black/30 rounded-2xl shadow-lg relative font-semibold text-lg mt-4 hoverable:hover:scale-110  js-map-container h-auto opacity-0 col-span-3" ref={overlayContainer}>
             <Image src={map} alt="地図のイラスト" className="h-72 w-auto mx-auto" />
-            <div className="top-4 left-4 absolute bg-white/70 dark:bg-black/50 rounded-lg shadow-lg p-2 text-center w-1/4 md:w-1/6 overflow-hidden h-16 will-change-auto max-sm:w-1/3">
+            <div className="top-4 left-4 absolute bg-white/70 dark:bg-black/50 rounded-lg shadow-lg p-2 text-center w-1/4 overflow-hidden h-16 will-change-auto max-sm:w-1/3">
               経済
               <span className="block" ref={times.economics}>{overlay.economics}</span>
             </div>
-            <div className="top-4 right-4 absolute bg-white/70 dark:bg-black/50 rounded-lg shadow-lg p-2 text-center w-1/4 md:w-1/6 overflow-hidden h-16 will-change-auto max-sm:w-1/3">
+            <div className="top-4 right-4 absolute bg-white/70 dark:bg-black/50 rounded-lg shadow-lg p-2 text-center w-1/4 overflow-hidden h-16 will-change-auto max-sm:w-1/3">
               社・現福
               <span className="block" ref={times.health}>{overlay.health}</span>
             </div>
-            <div className="bottom-4 left-4 absolute bg-white/70 dark:bg-black/50 rounded-lg shadow-lg p-2 text-center w-1/4 md:w-1/6 overflow-hidden h-16 will-change-auto max-sm:w-1/3">
+            <div className="bottom-4 left-4 absolute bg-white/70 dark:bg-black/50 rounded-lg shadow-lg p-2 text-center w-1/4 overflow-hidden h-16 will-change-auto max-sm:w-1/3">
               体育館
               <span className="block" ref={times.gym}>{overlay.gym}</span>
             </div>
-            <div className="bottom-4 right-4 absolute bg-white/70 dark:bg-black/50 rounded-lg shadow-lg p-2 text-center w-1/4 md:w-1/6 overflow-hidden h-16 will-change-auto max-sm:w-1/3">
+            <div className="bottom-4 right-4 absolute bg-white/70 dark:bg-black/50 rounded-lg shadow-lg p-2 text-center w-1/4 overflow-hidden h-16 will-change-auto max-sm:w-1/3">
               スポ健康
               <span className="block" ref={times.sport}>{overlay.sport}</span>
             </div>
           </div>
 
           {/* 三つ目のカード */}
-          <div className="font-semibold text-lg flex mt-2 justify-center w-full bg-white/20 dark:bg-black/30 rounded-2xl shadow-lg p-2 text-center hoverable:hover:scale-110 opacity-0">
-          {state.station==="nishihachioji" ? <button className="bg-black/80 dark:bg-white/80 rounded-xl px-1 py-3 w-1/3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
+          <div className="font-semibold text-lg mt-2 justify-center w-full bg-white/20 dark:bg-black/30 rounded-2xl shadow-lg p-2 text-center hoverable:hover:scale-110 opacity-0 grid grid-cols-3 col-span-3">
+            {state.station === "nishihachioji" ? <button className="bg-black/80 dark:bg-white/80 rounded-xl px-1 py-3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
               handleStationButtonClicked("nishihachioji")
-            }} ref={stationRefs.nishihachioji}>西八王子</button> : <button className="bg-black/50 dark:bg-white/50 rounded-xl px-1 py-3 w-1/3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
+            }} ref={stationRefs.nishihachioji}>西八王子</button> : <button className="bg-black/50 dark:bg-white/50 rounded-xl px-1 py-3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
               handleStationButtonClicked("nishihachioji")
             }} ref={stationRefs.nishihachioji}>西八王子</button>}
-            {state.station==="mejirodai" ? <button className="bg-black/80 dark:bg-white/80 rounded-xl px-1 py-3 w-1/3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
+            {state.station === "mejirodai" ? <button className="bg-black/80 dark:bg-white/80 rounded-xl px-1 py-3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
               handleStationButtonClicked("mejirodai")
-            }} ref={stationRefs.mejirodai}>めじろ台</button> : <button className="bg-black/50 dark:bg-white/50 rounded-xl px-1 py-3 w-1/3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
+            }} ref={stationRefs.mejirodai}>めじろ台</button> : <button className="bg-black/50 dark:bg-white/50 rounded-xl px-1 py-3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
               handleStationButtonClicked("mejirodai")
             }} ref={stationRefs.mejirodai}>めじろ台</button>}
-            {state.station==="aihara" ? <button className="bg-black/80 dark:bg-white/80 rounded-xl px-1 py-3 w-1/3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
+            {state.station === "aihara" ? <button className="bg-black/80 dark:bg-white/80 rounded-xl px-1 py-3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
               handleStationButtonClicked("aihara")
-            }} ref={stationRefs.aihara}>相原</button> : <button className="bg-black/50 dark:bg-white/50 rounded-xl px-1 py-3 w-1/3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
+            }} ref={stationRefs.aihara}>相原</button> : <button className="bg-black/50 dark:bg-white/50 rounded-xl px-1 py-3 shadow-lg scale-90 text-white dark:text-black" onClick={() => {
               handleStationButtonClicked("aihara")
             }} ref={stationRefs.aihara}>相原</button>}
           </div>
-          
           {/* 割引ボタン */}
           <Link
             href="/discount"
-            className="my-4 rounded-full bg-gradient-to-r from-pink-400 to-sky-300 font-bold text-3xl text-center p-5 w-full block border-gray-800 shadow-lg text-black border-2 opacity-0"
+            className="my-4 rounded-full bg-gradient-to-r from-pink-400 to-sky-300 font-bold text-3xl text-center p-5 w-full block border-gray-800 shadow-lg text-black border-2 opacity-0 col-span-2"
             ref={waribikiRef}>
             飲食店割引はこちら
           </Link>
+        </div>
           <p className="text-black text-center font-medium mt-2 mx-auto">時刻は目安であり、交通状況等による変わる可能性があります。<br />また臨時便等には対応しておりません。</p>
           <p className="text-center text-black">©CODE MATES︎</p>
-        </div>
       </div>
     </>
   );
