98eda43ed475dd397352e75c7f95668d7b34faa8
tommy10092525 <tommyxla777@gmail.com>
2025-03-15 18:19:16 +0900

    店舗カードの画像にアスペクト比を追加して、表示を改善

--- src/components/ui/store-card.tsx
+++ src/components/ui/store-card.tsx
@@ -11,7 +11,7 @@ type StoreCardProps = {
 const StoreCard = ({storeName,storeImage,storeDescription,url,children}:StoreCardProps) => {
   return (
     <Link href={url} className='bg-gray-100 dark:bg-zinc-900 shadow-lg dark:border border-0 dark:border-zinc-700 rounded-lg hover:scale-105 transition-all duration-300'>
-      <Image width={1000} height={1000} src={storeImage} alt={`${storeName}の画像`} className='mx-auto rounded-t-lg'/>
+      <Image width={1000} height={1000} src={storeImage} alt={`${storeName}の画像`} className='mx-auto rounded-t-lg aspect-[16/9]'/>
       <div className='text-center'>
         <p className='text-md'>{storeDescription}</p>
         <p className='font-bold text-3xl'>{storeName}</p>
