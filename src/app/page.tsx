e34eff3a04c2cc56b3f247298395fa74b8130236
tommy10092525 <tommyxla777@gmail.com>
2025-03-16 20:28:33 +0900

    useGSAPの依存関係をstate.isComingToHoseiとstate.stationに変更して、アニメーションのトリガーを改善

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
   }, []);
   useGSAP(()=>{
     animateText()
-  },[state])
+  },[state.isComingToHosei,state.station])
   useGSAP(()=>{
     animateDirectionButton()
     animateArrows()
